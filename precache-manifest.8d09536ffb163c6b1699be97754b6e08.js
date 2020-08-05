9cce9228dd5ae0e60a9ee746ff5bc350186207ef
Vaibhav Sinha <vaibhavleoanto@gmail.com>
2020-08-05 23:58:27 +0530

    Updates

---
+++ precache-manifest.8d09536ffb163c6b1699be97754b6e08.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eff56bda6d56ecd876ab905376d409",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85e46629346e78024532",
+    "url": "/portfolio/static/css/2.7c21bc47.chunk.css"
+  },
+  {
+    "revision": "1b81d716fb57f0ac583a",
+    "url": "/portfolio/static/css/main.e0622203.chunk.css"
+  },
+  {
+    "revision": "85e46629346e78024532",
+    "url": "/portfolio/static/js/2.a98f74de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.a98f74de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b81d716fb57f0ac583a",
+    "url": "/portfolio/static/js/main.3a8573aa.chunk.js"
+  },
+  {
+    "revision": "4c76482b968eb9f47af6",
+    "url": "/portfolio/static/js/runtime-main.98fb296d.js"
+  },
+  {
+    "revision": "371ee866a7e311f9e0ecadc2d73606b9",
+    "url": "/portfolio/static/media/certificate.371ee866.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3cb2a9855189f5285ffa29ae926d8410",
+    "url": "/portfolio/static/media/vaibh.3cb2a985.png"
+  }
+]);
